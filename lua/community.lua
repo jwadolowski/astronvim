4212a1474b3af426ca22cde33136316267127902
Jakub Wadolowski <jakub@wadolowski.tech>
2024-05-06 12:10:09 +0200

    feat: community file migrated over from the old config (with a few extra bits and pieces)

--- lua/community.lua
+++ lua/community.lua
@@ -1,12 +1,61 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
 
 ---@type LazySpec
 return {
+  -- default
   "AstroNvim/astrocommunity",
+
+  -- color
+  { import = "astrocommunity.color.tint-nvim" },
+  -- { import = "astrocommunity.color.vim-highlighter" },
+
+  -- colorshemes
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+
+  -- debugging
+  { import = "astrocommunity.debugging.nvim-bqf" },
+
+  -- editing-support
+  { import = "astrocommunity.editing-support.nvim-treesitter-context" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  { import = "astrocommunity.editing-support.suda-vim" },
+  { import = "astrocommunity.editing-support.yanky-nvim" },
+
+  -- media
+  { import = "astrocommunity.media.codesnap-nvim" },
+
+  -- motion
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.motion.flash-nvim" },
+
+  -- pack
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.cue" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.helm" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.just" },
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.pkl" },
+  { import = "astrocommunity.pack.proto" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.ruby" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.yaml" },
+
+  -- syntax
+  { import = "astrocommunity.syntax.hlargs-nvim" },
+
+  -- utility
+  { import = "astrocommunity.utility.noice-nvim" },
 }
