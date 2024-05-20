790f1a4fc3604187d3604837ffd257b1f28ff145
Jakub Wadolowski <jakub@wadolowski.tech>
2024-05-20 13:16:23 +0200

    feat(none-ls): Use Google's shell styleguide. Enable Markdown formatting via prettierd

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize None-ls sources
 
 ---@type LazySpec
@@ -7,7 +5,7 @@ return {
   "nvimtools/none-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -16,6 +14,21 @@ return {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
+
+      -- shfmt options:
+      -- -i,  --indent uint       0 for tabs (default), >0 for number of spaces
+      -- -ci, --case-indent       switch cases will be indented
+      --
+      -- Refs:
+      -- - https://google.github.io/styleguide/shellguide.html
+      null_ls.builtins.formatting.shfmt.with {
+        extra_args = { "-i", "2", "-ci" },
+        -- shfmt works just fine for Bash-like .zsh files too
+        filetypes = { "sh", "zsh" },
+      },
+
+      -- required to format markdown files
+      null_ls.builtins.formatting.prettierd,
     }
     return config -- return final config table
   end,
