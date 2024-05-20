9e1863772c5f88b81bbc99262a520cd0e682024c
Jakub Wadolowski <jakub@wadolowski.tech>
2024-05-20 14:03:10 +0200

    feat(polish): Custom filetype mappings

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,3 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
@@ -7,12 +5,43 @@ if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 -- Set up custom filetypes
 vim.filetype.add {
   extension = {
-    foo = "fooscript",
+    hurl = "hurl",
+    -- https://jsonlines.org/
+    jsonl = "json",
   },
   filename = {
-    ["Foofile"] = "fooscript",
+    -- ["Foofile"] = "fooscript",
   },
   pattern = {
-    ["~/%.config/foo/.*"] = "fooscript",
+    -- Lua does not support regex
+    --
+    -- https://stackoverflow.com/a/2696469/6802186
+    -- https://neovim.io/doc/user/luaref.html#luaref-patterns
+    --
+    -- '-' and '.' are "magic character" hence they have to be escaped
+    [".+/dispatcher/src/.+%.any"] = "apache",
+    [".+/dispatcher/src/.+%.conf"] = "apache",
+    [".+/dispatcher/src/.+%.farm"] = "apache",
+    [".+/dispatcher/src/.+%.rules"] = "apache",
+    [".+/dispatcher/src/.+%.vars"] = "apache",
+    [".+/dispatcher/src/.+%.vhost"] = "apache",
+    [".+/dispatcher%-sdk%-.+/src/.+%.any"] = "apache",
+    [".+/dispatcher%-sdk%-.+/src/.+%.conf"] = "apache",
+    [".+/dispatcher%-sdk%-.+/src/.+%.farm"] = "apache",
+    [".+/dispatcher%-sdk%-.+/src/.+%.rules"] = "apache",
+    [".+/dispatcher%-sdk%-.+/src/.+%.vars"] = "apache",
+    [".+/dispatcher%-sdk%-.+/src/.+%.vhost"] = "apache",
+    -- Renderend k8s manifests should be treated as plain YAML files
+    --
+    -- To avoid clashes with Helm pack (see below) an explicit priority was defined.
+    --
+    -- Refs:
+    -- - https://github.com/AstroNvim/astrocommunity/blob/main/lua/astrocommunity/pack/helm/init.lua
+    -- - https://neovim.io/doc/user/lua.html#vim.filetype
+    [".+/rendered%-manifests/.+/.+%.ya?ml"] = { "yaml", { priority = 10 } },
+    -- YAML files with custom extension:
+    -- - foo.yaml.ci.tpl
+    -- - bar.yaml.ci.tpln
+    [".+%.ya?ml%.ci%.tpln?"] = { "yaml" },
   },
 }
