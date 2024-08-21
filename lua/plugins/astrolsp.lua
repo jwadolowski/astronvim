682d0221381b58abadf684f2cd7c0124548bfdf4
jwadolowski <6334715+jwadolowski@users.noreply.github.com>
2024-08-21 12:52:00 +0200

    feat(astrolsp): Disable XML auto-formatting

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -25,6 +25,7 @@ return {
         ignore_filetypes = { -- disable format on save for specified filetypes
           "json",
           "markdown",
+          "xml",
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
