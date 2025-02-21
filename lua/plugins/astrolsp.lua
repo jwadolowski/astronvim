913915543c7fefaf232b0053367ca3a6f260bf14
Jakub Wądołowski <6334715+jwadolowski@users.noreply.github.com>
2025-02-21 23:56:49 +0100

    feat(astrocore): Add Lua to autoformat-on-save files

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -22,6 +22,7 @@ return {
         allow_filetypes = { -- enable format on save for specified filetypes only
           "go",
           "terraform",
+          "lua",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
         },
