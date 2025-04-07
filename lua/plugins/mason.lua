b3bc78c27b57893d0cc6f0172bbccafca6fc3f50
Jakub Wądołowski <6334715+jwadolowski@users.noreply.github.com>
2025-04-07 22:06:50 +0200

    fix(mason): Remove deprecated buf-language-server

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -6,8 +6,11 @@ return {
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     opts = function(_, opts)
-      -- Remove tfsec from ensure_installed if it exists
-      opts.ensure_installed = vim.tbl_filter(function(tool) return tool ~= "tfsec" end, opts.ensure_installed or {})
+      -- Remove tfsec and buf-language-server from ensure_installed if they exist
+      opts.ensure_installed = vim.tbl_filter(
+        function(tool) return tool ~= "tfsec" and tool ~= "buf-language-server" end,
+        opts.ensure_installed or {}
+      )
     end,
   },
 }
