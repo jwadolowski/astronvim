6f45de9b5a13cbd15efce224d716989d14c09a00
Jakub Wądołowski <6334715+jwadolowski@users.noreply.github.com>
2025-05-18 19:01:31 +0200

    fix(mason): Remove invalid buf_ls package

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,12 +3,14 @@
 ---@type LazySpec
 return {
   -- use mason-tool-installer for automatically installing Mason packages
+  --
+  -- https://github.com/AstroNvim/astrocommunity/pull/1508
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     opts = function(_, opts)
-      -- Remove tfsec and buf-language-server from ensure_installed if they exist
+      -- Remove tfsec and buf_ls from ensure_installed if they exist
       opts.ensure_installed = vim.tbl_filter(
-        function(tool) return tool ~= "tfsec" and tool ~= "buf-language-server" end,
+        function(tool) return tool ~= "tfsec" and tool ~= "buf_ls" end,
         opts.ensure_installed or {}
       )
     end,
