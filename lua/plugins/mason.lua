fe8f7c867172a2dbba74955ff83031ada189671d
Jakub Wądołowski <6334715+jwadolowski@users.noreply.github.com>
2025-05-19 12:58:21 +0200

    fix(mason): Simplify WhoIsSethDaniel/mason-tool-installer.nvim exclusions

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,16 +3,11 @@
 ---@type LazySpec
 return {
   -- use mason-tool-installer for automatically installing Mason packages
-  --
-  -- Remove buf_ls once https://github.com/AstroNvim/astrocommunity/pull/1508 is merged
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     opts = function(_, opts)
-      -- Remove tfsec and buf_ls from ensure_installed if they exist
-      opts.ensure_installed = vim.tbl_filter(
-        function(tool) return tool ~= "tfsec" and tool ~= "buf_ls" end,
-        opts.ensure_installed or {}
-      )
+      -- Remove tfsec from ensure_installed if they exist
+      opts.ensure_installed = vim.tbl_filter(function(tool) return tool ~= "tfsec" end, opts.ensure_installed or {})
     end,
   },
 }
